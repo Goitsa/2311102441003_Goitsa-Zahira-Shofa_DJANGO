37ae9ffbbfb221f2210bece12d917930096aa8c1
Gesa <2311102441003@umkt.ac.id>
2025-04-16 20:10:52 +0800

    - Menambahkan Template dan Halaman dashboard

--- main/urls.py
+++ main/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
@@ -26,7 +26,7 @@ from main.views import home, about
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('about/', about, name='about'),
+    path('dashboard/', include("berita.urls")),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
